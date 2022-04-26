f4dbf973b72c0940ec2d369b792c4f9373287d53
renatatopinkova <r.topinkova@gmail.com>
2022-04-26 19:06:29 +0100

    Initial commit

---
+++ app.R
@@ -0,0 +1,69 @@
+
+
+# Load libraries ----------------------------------------------------------
+
+
+library(shiny)
+library(dplyr)
+library(stringr)
+library(DT)
+
+
+# Import data -------------------------------------------------------------
+
+readRDS("df_thoughts_long")
+readRDS("labels")
+
+
+
+
+# App ---------------------------------------------------------------------
+
+
+# Client-side
+ui <- fluidPage(
+  # Application title
+  titlePanel("Topic explorer"),
+
+  # Sidebar with a topic selector
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("topic_number",
+        "Select topic number",
+        choices = seq(1:70)
+      ),
+      textOutput("heading")
+    ),
+
+    # Show table with sample texts
+    mainPanel(
+      tableOutput("topic_table")
+    )
+  )
+)
+
+# Server-side
+server <- function(input, output) {
+
+  # define table
+  output$topic_table <- renderTable({
+    # get the right column based on input
+    df_thoughts_long %>%
+      # filter topic number
+      filter(topic == input$topic_number) %>%
+      # discard topic column
+      select(-topic) %>%
+      # add row number
+      mutate(id = row_number()) %>%
+      # move it to the front
+      relocate(id, .before = title)
+  })
+
+  # get heading with topic frex terms
+  output$heading <- renderText({
+    paste0("Topic ", input$topic_number, ": ", str_c(labels$frex[eval(parse(text = input$topic_number)), 1:10], collapse = ", "))
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
